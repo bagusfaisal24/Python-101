8ef4b545073ac3e4064aa047edb981a968ad5318
bagus faisal <faizalbagus00@gmail.com>
2020-02-18 14:23:36 +0700

    menambah belajar tentang operator

--- Data Type/DataTypes.py
+++ Data Type/DataTypes.py
@@ -1,8 +1,8 @@
-x1 = int(20) # int
-x2 = float(20.5) # float
-x3 = complex(1j) # complex
-x4 = list(("apple","mango","banana")) # list
-x5 = tuple(("apple", "mango", "banana")) # tuple
+x1 = int(20)  # int
+x2 = float(20.5)  # float
+x3 = complex(1j)  # complex
+x4 = list(("apple", "mango", "banana"))  # list
+x5 = tuple(("apple", "mango", "banana"))  # tuple
 x6 = bool(0)
 y = 35656222554887711
 y1 = 2.35
@@ -12,3 +12,19 @@ print(type(y1))
 print(type(x6))
 print(type(x5))
 print(type(x4))
+
+## Casting data type
+print(int(1))
+print(float(2.8))
+print(str(2.8) + str(2.8))
+
+## boolean logical
+a = 300
+b = 200
+
+if a > b:
+    print("true")
+else:
+    print("false")
+
+
---
+++ Functional/Operators.py
@@ -0,0 +1,32 @@
+a = float(1.8)
+b = 2
+penjumlahan = a + b
+pengurangan = b - a
+perkalian = a * b
+pembagian = 8 / 4
+modulus = 8 % 2 == 0
+floor_div = 15 // 2
+print(penjumlahan)
+print(pengurangan)
+print(perkalian)
+print(pembagian)
+print(modulus)
+print(floor_div)
+
+## assignment operator
+x = 5
+x += 3
+x *= 2
+print(x)
+
+## equal number
+if x <= 5 or x >= 5:
+    print("true")
+elif not (x != 5):
+    print("false")
+else:
+    print("true")
+
+## equal string
+if 'bagus' == 'bagus':
+    print("true")
