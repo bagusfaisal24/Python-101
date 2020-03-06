c9dbe3c68676c37d4fe74a3597152c2020ec30b9
bagus faisal <faizalbagus00@gmail.com>
2020-03-06 14:50:32 +0700

    oop python

---
+++ object-oriented/Person.py
@@ -0,0 +1,9 @@
+class Person:
+    ##define object
+    def __init__(self, name, age):
+       self.name = name
+       self.age=age
+
+object1 = Person("Bagus","25")
+print(object1.name)
+print(object1.age)
