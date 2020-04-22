3f1dfead046174b3395d2e01fbd4128834da9438
bagus faisal kh <bagus.faizal@dssconsulting.id>
2020-04-22 14:38:45 +0700

    tambah case

--- Case/CodingBatChallenge.py
+++ Case/CodingBatChallenge.py
@@ -5,7 +5,6 @@ def sleep_in(weekday, vacation):
     else:
         print('sleepin', False)
 
-
 sleep_in(False, True)
 
 
@@ -18,5 +17,28 @@ def monkey_trouble(a_smile, b_smile):
     else:
         print('not trouble', False)
 
-
 monkey_trouble(True, False)
+
+
+## sum_double
+def sum_double(a, b):
+    if a != b:
+        print('sum', a + b)
+    elif a == b:
+        print('sum double', (a + b) * 2)
+
+sum_double(2, 2)
+
+
+## dif21
+def dif21(n):
+    if n < 21:
+        print('dif21', 21 - n)
+    else:
+        print('dif21', (n - 21) * 2)
+
+dif21(2)
+dif21(10)
+dif21(21)
+dif21(25)
+## Sumber codingbat.com
