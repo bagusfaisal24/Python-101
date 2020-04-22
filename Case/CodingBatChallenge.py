3fc36f184a63a98d5975c285846ac209a4eb821b
bagus faisal kh <bagus.faizal@dssconsulting.id>
2020-04-22 14:41:35 +0700

    add deskripsi

--- Case/CodingBatChallenge.py
+++ Case/CodingBatChallenge.py
@@ -1,4 +1,6 @@
 ## sleep_in
+# The parameter weekday is True if it is a weekday, and the parameter vacation is True if we are on vacation.
+# We sleep in if it is not a weekday or we're on vacation. Return True if we sleep in.
 def sleep_in(weekday, vacation):
     if not weekday or vacation:
         print('sleepin', True)
@@ -6,9 +8,13 @@ def sleep_in(weekday, vacation):
         print('sleepin', False)
 
 sleep_in(False, True)
+sleep_in(True, True)
+sleep_in(False, True)
 
 
 ## monkey trouble
+# We have two monkeys, a and b, and the parameters a_smile and b_smile indicate if each is smiling.
+# We are in trouble if they are both smiling or if neither of them is smiling. Return True if we are in trouble.
 def monkey_trouble(a_smile, b_smile):
     if a_smile and b_smile:
         print('monkey trouble', True)
@@ -18,9 +24,12 @@ def monkey_trouble(a_smile, b_smile):
         print('not trouble', False)
 
 monkey_trouble(True, False)
+monkey_trouble(True, True)
+monkey_trouble(False, True)
 
 
 ## sum_double
+# Given two int values, return their sum. Unless the two values are the same, then return double their sum.
 def sum_double(a, b):
     if a != b:
         print('sum', a + b)
@@ -31,6 +40,7 @@ sum_double(2, 2)
 
 
 ## dif21
+# Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
 def dif21(n):
     if n < 21:
         print('dif21', 21 - n)
@@ -41,4 +51,5 @@ dif21(2)
 dif21(10)
 dif21(21)
 dif21(25)
+
 ## Sumber codingbat.com
